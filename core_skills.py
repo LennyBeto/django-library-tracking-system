5d4f6bf61363bc418ddf6aeebc0ab629e5225e17
Lenny Beto <lennybeto95@gmail.com>
2026-02-10 17:37:51 +0300

    feat: updated core_skills.py file for list generation

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,9 @@
 import random
-# rand_list =
+# rand_list = 
+rand_list = [random.randint(1,100) for _ in range (10)]
+print (f"Random list: {rand_list}")
 
 # list_comprehension_below_10 =
-
+list_comprehension_below_10 = [x for x in rand_list if x < 10]
+print (f "Numbers below 10: {list comprehension below 10}")
 # list_comprehension_below_10 =
